a7596a4820b9ecaef1bc20332049356c7666f28c
Latifoo_Djr <abdellatif.djermouni@mf.gov.dz>
2025-05-21 10:48:36 +0200

    Final Commit

---
+++ src/main/java/com/API/Documents_Management/Components/DatabaseCleanupScheduler.java
@@ -0,0 +1,24 @@
+package com.API.Documents_Management.Components;
+
+import com.API.Documents_Management.Services.CourrielService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class DatabaseCleanupScheduler {
+
+    private final CourrielService courrielService;
+
+    // Methode will be executed every thursday at 15h30
+
+    @Scheduled(cron = "0 30 15 ? * THU", zone = "Africa/Algiers")
+    public void scheduledDatabaseCleanup() {
+        System.out.println("[SCHEDULER] " + LocalDateTime.now() + " - Cleaning orphan files and courriels from database...");
+        courrielService.cleanDatabaseFromMissingDiskData();
+        System.out.println("[SCHEDULER] " + LocalDateTime.now() + " - Cleanup finished.");
+    }
+}
