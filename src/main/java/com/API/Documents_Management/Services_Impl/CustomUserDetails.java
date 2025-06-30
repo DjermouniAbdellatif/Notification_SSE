b30220ea1205f7876b4f5a49d737784072a441a4
Latifoo_Djr <abdellatif.djermouni@mf.gov.dz>
2025-06-30 12:09:55 +0200

    Add Notification by hierarch

---
+++ src/main/java/com/API/Documents_Management/Services_Impl/CustomUserDetails.java
@@ -0,0 +1,62 @@
+package com.API.Documents_Management.Services_Impl;
+
+import com.API.Documents_Management.Entities.AppUser;
+import com.API.Documents_Management.WebSocket.NotificationEntity;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private final AppUser user;
+    private final Set<GrantedAuthority> authorities;
+
+    public CustomUserDetails(AppUser user, Set<GrantedAuthority> authorities) {
+        this.user = user;
+        this.authorities = authorities;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+}
